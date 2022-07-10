0ba7512808e4e9b3889103abeae96fd95c9211c8
dulabdul <dul.abdoel21@gmail.com>
2022-07-11 04:50:15 +0700

    Add Image and text header

--- src/elements/pages/LandingPage.js
+++ src/elements/pages/LandingPage.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Header from "elements/parts/Header";
+import landingPage from "json/landingPage.json";
+import Hero from "elements/parts/Hero";
 class LandingPage extends React.Component {
-	render() {
-		return (
-			<>
-				<Header {...this.props}></Header>
-			</>
-		);
-	}
+  render() {
+    return (
+      <>
+        <Header {...this.props}></Header>
+        <Hero data={landingPage.hero} />
+      </>
+    );
+  }
 }
 
 export default LandingPage;
--- src/elements/parts/BrandText.js
+++ src/elements/parts/BrandText.js
@@ -1,12 +1,10 @@
-import React from 'react'
+import React from "react";
 import Button from "elements/button";
 
 export default function BrandText() {
   return (
-    <Button className='brand-icon-text' type='link' href=''>
-        Trip <span className='text-gray-900'>Vacation</span>
+    <Button className="brand-icon-text fw-bold" type="link" href="">
+      Trip<span className="text-gray-900">Vacation.</span>
     </Button>
-  )
+  );
 }
-
-
