770d8be2bccb113834620e5f3c8e08d3fa2098e8
dulabdul <dul.abdoel21@gmail.com>
2022-08-01 21:45:30 +0700

    Initial Setting SBAdmin2

---
+++ tripvacation-server/routes/admin.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+const adminController = require('../controller/adminController');
+router.get('/dashboard', adminController.viewDashboard);
+
+module.exports = router;
