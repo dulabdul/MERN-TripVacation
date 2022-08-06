e59a590543075862c82e5ac44f7bf125ae041e6c
dulabdul <dul.abdoel21@gmail.com>
2022-08-06 13:48:50 +0700

    Commit Fitur delete, update, adding category

--- tripvacation-server/app.js
+++ tripvacation-server/app.js
@@ -3,17 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/db_tripvacation');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const adminRouter = require('./routes/admin');
-
+const methodOverride = require('method-override');
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(methodOverride('_method'));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
