ba905d18fee8688dec4f3c08a14ae3f73ecc45cd
dulabdul <dul.abdoel21@gmail.com>
2022-08-07 14:00:01 +0700

    Update Routes JS

--- tripvacation-server/routes/admin.js
+++ tripvacation-server/routes/admin.js
@@ -1,14 +1,17 @@
 const router = require('express').Router();
 const adminController = require('../controller/adminController');
+const { upload } = require('../middlewares/multer');
 router.get('/dashboard', adminController.viewDashboard);
 router.get('/category', adminController.viewCategory);
 router.post('/category', adminController.addCategory);
 router.put('/category', adminController.editCategory);
 router.delete('/category/:id', adminController.deleteCategory);
 router.get('/bank', adminController.viewBank);
-router.put('/bank', adminController.editBank);
-router.post('/bank', adminController.addBank);
+router.put('/bank', upload, adminController.editBank);
+router.post('/bank', upload, adminController.addBank);
+router.delete('/bank/:id', adminController.deleteBank);
 router.get('/item', adminController.viewItem);
+router.post('/item', adminController.addItem);
 router.get('/booking', adminController.viewBooking);
 
 module.exports = router;
