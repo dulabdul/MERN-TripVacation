2ecfd891b49102aaf736ca22b5c32c752daa2429
unknown <dul.abdoel21@gmail.com>
2022-08-15 16:22:57 +0700

    test

--- tripvacation-server/app.js
+++ tripvacation-server/app.js
@@ -4,7 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/db_tripvacation');
+mongoose.connect(
+  'mongodb+srv://dulabdul:dulabdul1521@cluster0.t9lyffm.mongodb.net/db_tripvacation?retryWrites=true&w=majority'
+);
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const adminRouter = require('./routes/admin');
@@ -15,10 +17,10 @@ const session = require('express-session');
 const app = express();
 let time = new Date(Date.now() + (30 * 86400 + 1000));
 let sess = {
-	secret: 'keyboard_cat',
-	cookie: { maxAge: time },
-	resave: false,
-	saveUninitialized: true,
+  secret: 'keyboard_cat',
+  cookie: { maxAge: time },
+  resave: false,
+  saveUninitialized: true,
 };
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -30,8 +32,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
-	'/sb-admin-2',
-	express.static(path.join(__dirname, 'node_modules/startbootstrap-sb-admin-2'))
+  '/sb-admin-2',
+  express.static(path.join(__dirname, 'node_modules/startbootstrap-sb-admin-2'))
 );
 app.use(session(sess));
 app.use(flash());
@@ -42,18 +44,18 @@ app.use('/admin', adminRouter);
 app.use('/api/v1/member', apiRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-	next(createError(404));
+  next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-	// render the error page
-	res.status(err.status || 500);
-	res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
